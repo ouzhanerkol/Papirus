ceb23e147f03b36f2485f9c3f454c32822fdb663
ouzhanerkol <oguzhannerkol@gmail.com>
2017-07-18 23:08:24 +0300

    Initial commit

---
+++ src/mainFrame.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class mainFrame {
+    public static void main(String{} args){
+        JFrame mainFrame = new JFrame("Papirus");
+        // okay- bekle - okay- bi saniye
+        mainFrame.pack();
+        mainFrame.setVisible(true);
+    }
+}
