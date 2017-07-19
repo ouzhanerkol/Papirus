0efb5bca25ed70224da6f03148d0ad2b78ebc7ec
Çağatay Uslu <cagatayuslu@msn.com>
2017-07-20 01:24:41 +0300

    GUI Process

---
+++ src/GUIex.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+/**
+ * Created by Çağatay Uslu on 20.07.2017.
+ */
+public class GUIex {
+    private JPanel panel1;
+    private JLabel logo;
+    private JPanel panel;
+    private JLabel play;
+    private JLabel ex;
+}
