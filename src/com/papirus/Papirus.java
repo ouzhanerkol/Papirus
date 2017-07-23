2b6e9662d32dfccea5781bc839e6a42e000fc0a7
Çağatay Uslu <cagatayuslu@msn.com>
2017-07-23 03:38:29 +0300

    GUI Process

--- src/com/papirus/Papirus.java
+++ src/com/papirus/Papirus.java
@@ -10,11 +10,10 @@ public class Papirus {
 
     public static String EXIT_ICON = "",
             PAPIRUS_LOGO = "assets/images/papirus_logo.png",
-            PLAY_LOGO_64 = "assets/images/play_black-64x64.png",
-            INFO_LOGO_24 = "assets/images/exclamation-24x24.png",
-            INFO_LOGO_32 = "assets/images/exclamation-32x32.png",
-            OPTION_LOGO_24 = "assets/images/options-24x24.png",
-            OPTION_LOGO_32 = "assets/images/options-32x32.png";
+            PLAY_LOGO_64 = "assets/images/play_b4x64.png",
+            INFO_LOGO_24 = "assets/images/exclam_png",
+            OPTION_LOGO_24 = "assets/images/option4.png",
+            OPTION_LOGO_32 = "assets/images/optio2x32.png";
 
     public Papirus() {
     }
